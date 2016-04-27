92c9a48b58a121bd8796443494c0e732940f8b0b
moneyinto <woyaoqianjin@gmail.com>
2016-04-27 13:04:10 +0800

    add gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,32 @@ var minifyCss   = require('gulp-minify-css');
 var babel       = require('gulp-babel');
 var uglify      = require('gulp-uglify');
 var rename      = require('gulp-rename');
+var plumber     = require('gulp-plumber');
 
 gulp.task('jade', function () {
     return gulp.src(['src/*.jade', 'src/view/*.jade', 'src/view/**/*.jade'], {
         base: 'src'
     })
+        .pipe(plumber(function (error) {
+            console.log('----------------------------------------');
+            console.log(error);
+            console.log('----------------------------------------');
+        }))
         .pipe(jade())
+        .pipe(plumber.stop())
         .pipe(gulp.dest('dist'))
 });
 
 gulp.task('less', function () {
     return gulp.src(['src/less/*.less', 'src/less/**/*.less'])
+        .pipe(plumber(function (error) {
+            console.log('----------------------------------------');
+            console.log(error);
+            console.log('----------------------------------------');
+        }))
         .pipe(less())
         .pipe(minifyCss())
+        .pipe(plumber.stop())
         .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest('dist/css/'))
 });
@@ -27,10 +40,16 @@ gulp.task('js', function () {
     return gulp.src(['src/js/*.js', 'src/js/**/*.js'],{
             base: 'src'
         })
+        .pipe(plumber(function (error) {
+            console.log('----------------------------------------');
+            console.log(error);
+            console.log('----------------------------------------');
+        }))
         .pipe(babel({
             presets: ['babel-preset-es2015']
         }))
         .pipe(uglify())
+        .pipe(plumber.stop())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('dist'))
 });
@@ -38,7 +57,7 @@ gulp.task('js', function () {
 gulp.task('serve', ['less', 'js', 'jade'], function () {
     browserSync({
         notify: false,
-        files: ["dist/**/*", "dist/*"],         // 监听文件内容改变，刷新页面
+        files: ["dist/**/**/*", "dist/**/*", "dist/*"],         // 监听文件内容改变，刷新页面
         port: 3000,
         server: {
             baseDir: "./dist/"
